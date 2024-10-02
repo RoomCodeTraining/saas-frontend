d03463a1bc91cbfea263ead8de7fbff55091d5fe
fanebrahima <brahimafane06@gmail.com>
2024-10-02 14:13:15 +0000

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/eatci-front'),
+      dir: require('path').join(__dirname, './coverage/coop-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eatci-front'`, () => {
+  it(`should have as title 'coop-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eatci-front');
+    expect(app.title).toEqual('coop-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('eatci-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('coop-front app is running!');
   });
 });
