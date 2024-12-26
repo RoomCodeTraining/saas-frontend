73a1cb30dfb0a636a47970fbd0826751dde83abc
fanebrahima <brahimafane06@gmail.com>
2024-12-26 12:21:52 +0000

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
   // PREPRODUCTION
-  // api_url: 'https://saas.roomcodetraining.com/api/v1',
-  api_url: 'https://coop-back.ddev.site/api/v1',
+  api_url: 'https://saas.roomcodetraining.com/api/v1',
+  // api_url: 'https://coop-back.ddev.site/api/v1',
 
   statistic_default_date_from: '2024-01-01'
 }; 
